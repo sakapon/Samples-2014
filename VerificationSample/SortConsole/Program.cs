f2c2c83f241e914cb404d445d94574fba7073543
Keiho Sakapon <github@saka-pon.net>
2014-08-14 15:52:57 +0900

    Implement Sort method

--- VerificationSample/SortConsole/Program.cs
+++ VerificationSample/SortConsole/Program.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SortConsole
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            Console.WriteLine(Sort(new TwoValues(1, 1)));
+            Console.WriteLine(Sort(new TwoValues(1, 2)));
+            Console.WriteLine(Sort(new TwoValues(2, 1)));
+        }
+
+        // Point: 戻り値に対する高度な制約。
+        static OrderedTwoValues Sort(TwoValues v) // where Sort(v).SetEquals(v)
+        {
+            // Point: 変数の大小関係などの高度なコンテキスト。
+            // コンパイルが成功すれば、このメソッドの実装も成功です。
+            return v.X <= v.Y
+                ? new OrderedTwoValues(v.X, v.Y)
+                : new OrderedTwoValues(v.Y, v.X);
+        }
+    }
+
+    public class TwoValues
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public TwoValues(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool SetEquals(TwoValues v)
+        {
+            return X == v.X ? Y == v.Y
+                : X == v.Y && Y == v.X;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{{{0}, {1}}}", X, Y);
+        }
+    }
+
+    public class OrderedTwoValues : TwoValues
+    {
+        // Point: 引数に対する高度な制約。
+        public OrderedTwoValues(int x, int y)
+            : base(x, y) // where x <= y
         {
         }
     }
