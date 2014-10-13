962f0f2fb8126750bf2ae299ce61d6a6eac7874f
Keiho Sakapon <git@saka-pon.net>
2014-10-13 17:30:19 +0900

    Add test

--- MonadSample/MonadConsole/Program.cs
+++ MonadSample/MonadConsole/Program.cs
@@ -10,6 +10,43 @@ namespace MonadConsole
     {
         static void Main(string[] args)
         {
+            MonadTest();
+            MaybeTest();
+            FlowTest();
+        }
+
+        static void MonadTest()
+        {
+            var r1 =
+                from x in 0.ToMonad()
+                select x;
+
+            var r2 =
+                from x in 1.ToMonad()
+                from y in 2.ToMonad()
+                select x + y;
+        }
+
+        static void MaybeTest()
+        {
+            var r1 =
+                from x in 2.ToMaybe()
+                where x % 3 == 1
+                select x + 1;
+
+            var r2 =
+                from x in 1.ToMaybe()
+                from y in 2.ToMaybe()
+                where x % 3 == 1
+                select x + y;
+        }
+
+        static void FlowTest()
+        {
+            var r1 =
+                from x in 1.ToFlow()
+                from y in 0.ToFlow()
+                select x / y;
         }
     }
 }
