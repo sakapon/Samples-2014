932d06bdd2743a16cdddf4ac881a5a97ffc7575b
Keiho Sakapon <git@saka-pon.net>
2014-11-16 23:37:21 +0900

    Add Polynomial structure

---
+++ MathSample/EquationConsole/Polynomial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EquationConsole
+{
+    public struct Polynomial
+    {
+        public static implicit operator Polynomial(double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Polynomial operator +(Polynomial p1, Polynomial p2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Polynomial operator -(Polynomial p1, Polynomial p2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Polynomial operator *(Polynomial p1, Polynomial p2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Polynomial operator /(Polynomial p, double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Polynomial operator +(Polynomial p)
+        {
+            return p;
+        }
+
+        public static Polynomial operator -(Polynomial p)
+        {
+            return -1 * p;
+        }
+
+        public double Substitute(double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SolveEquation(double operand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
