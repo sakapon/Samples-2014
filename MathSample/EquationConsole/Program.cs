d14e15f4b43bb839e93b5a37d4be884e790194c9
Keiho Sakapon <github@saka-pon.net>
2014-12-05 20:30:08 +0900

    Add test

--- MathSample/EquationConsole/Program.cs
+++ MathSample/EquationConsole/Program.cs
@@ -10,6 +10,24 @@ namespace EquationConsole
     {
         static void Main(string[] args)
         {
+            PolynomialTest();
+        }
+
+        static void PolynomialTest()
+        {
+            var x = Polynomial.X;
+
+            var p1 = x + 2;
+            var p2 = x - 2;
+
+            var p3 = p1 + p2;
+            var p4 = p1 - p2;
+            var p5 = p1 * p2;
+            var p6 = p1 / 2;
+
+            Console.WriteLine(((Polynomial)0)[5]);
+            Console.WriteLine(p1[5]);
+            Console.WriteLine(p5[5]);
         }
     }
 }
