79098fbdc689d931512bcc03f20bd0f577ca956e
Keiho Sakapon <github@saka-pon.net>
2014-12-12 16:16:31 +0900

    Track ink gestures

--- InkSample/InkGestureWpf/MainWindow.xaml.cs
+++ InkSample/InkGestureWpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +25,22 @@ namespace InkGestureWpf
         public MainWindow()
         {
             InitializeComponent();
+
+            var recognizedGestures = new ObservableCollection<ApplicationGesture>();
+            RecognizedGesturesBox.ItemsSource = recognizedGestures;
+
+            // EditingMode プロパティを GestureOnly に設定するだけで、すべてのジェスチャが有効になります。
+            // ジェスチャを個別に指定したい場合、SetEnabledGestures メソッドを利用します。
+            //TheCanvas.SetEnabledGestures(new[] { ApplicationGesture.Circle, ApplicationGesture.Check });
+
+            TheCanvas.Gesture += (o, e) =>
+            {
+                var results = e.GetGestureRecognitionResults();
+                var result = results.FirstOrDefault(r => r.RecognitionConfidence == RecognitionConfidence.Strong);
+                if (result == null || result.ApplicationGesture == ApplicationGesture.NoGesture) return;
+
+                recognizedGestures.Insert(0, result.ApplicationGesture);
+            };
         }
     }
 }
