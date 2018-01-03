c7250725ee0ffe3c8caa3c6c7c144f857c9a7e62
Keiho Sakapon <git@saka-pon.net>
2018-01-04 00:24:18 +0900

    Move source code from VSTS

---
+++ ASPNET-MVC/SslSample/SslRedirect/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SslRedirect
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttps2Attribute(true));
+        }
+    }
+}
