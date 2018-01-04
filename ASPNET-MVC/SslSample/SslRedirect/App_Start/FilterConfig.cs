38aa64de1ee72dc61b2802f68d601fe518f8eb85
Keiho Sakapon <git@saka-pon.net>
2018-01-04 16:13:04 +0900

    Revert "Revert to initial project template"
    This reverts commit f918649ec9c0e7836515af190c304310ccd28fa4.

--- ASPNET-MVC/SslSample/SslRedirect/App_Start/FilterConfig.cs
+++ ASPNET-MVC/SslSample/SslRedirect/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace SslRedirect
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttps2Attribute(true));
         }
     }
 }
